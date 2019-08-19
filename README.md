8e633d062ec1ddfe1a808dec19cfa1099461cf93
huang puguang <puguang_huang@fpi-inc.com>
2019-08-19 11:35:22 +0800

    feat: add task

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # grunt-screeps-get
-A Grunt plugin for pull code from your Screeps account
+
+> A Grunt plugin for pull code from your Screeps account
+
+## Dependence
+
+- Grunt `~0.4.5`
+- node `>=8`
+
+
+## Getting Started
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-screeps-get --save-dev
+```
+
+## Usage Example
+
+**Gruntfile.js:**
+
+```js
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-screeps-get');
+
+    grunt.initConfig({
+        'screeps-get': {
+            default: {
+                options: {
+                    email: 'YOUR_EMAIL',
+                    password: 'YOUR_PASSWORD',
+                    branch: 'default',
+                    ptr: false
+                },
+                saveDir: 'src/'
+            }
+        }
+    });
+}
+```
+
+Now you can pull code to the folder `src/` from your Screeps account with following command:
+
+```
+grunt screeps-get
+```
+
+## Problem
+
+This plugin does not support recognizing `warm` files, all files will be interpreted as `js` files.
---
+++ tasks/screeps-get.js
@@ -0,0 +1,59 @@
+/*
+ * grunt-screeps-get
+ * https://github.com/HoPGoldy/grunt-screeps-get
+ *
+ * Copyright (c) 2019 hopgoldy
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+const fs = require('fs');
+const https = require('https');
+
+module.exports = function(grunt) {
+  grunt.registerMultiTask('screeps-get', 'A Grunt plugin for pull code from your Screeps account', function() {
+    const done = this.async();
+    const options = this.options({});
+    // 生成请求路径
+    const requestPath = (options.ptr ? '/ptr/api/user/code' : '/api/user/code') + `?branch=${options.branch || 'default'}`;
+    // 生成保存文件夹
+    const saveDir = this.data.saveDir;
+    // 组装请求 options
+    const requestOptions = {
+      hostname: 'screeps.com',
+      port: 443,
+      path: requestPath,
+      method: 'GET',
+      auth: `${options.email}:${options.password}`
+    };
+
+    const req = https.request(requestOptions, res => {
+      res.setEncoding('utf8');
+      let data = '';
+
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        data = JSON.parse(data)
+        // 错误处理
+        if (!data.ok) return console.error('Error while pulling from Screeps.\n', data); 
+        
+        // 新建文件夹
+        if (!fs.existsSync(saveDir)) {
+          fs.mkdirSync(saveDir);
+        }
+
+        // 创建文件
+        for (const moduleName in data.modules) {
+          fs.writeFile(`${saveDir}/${moduleName}.js`, data.modules[moduleName], err => {
+            if (err) return console.log('Error with writeFile ', err);
+          });
+        }
+        done();
+      })
+    })
+
+    req.on('error', e => console.error(`Error with request: ${e.message}`));
+    req.end();
+  });
+};
